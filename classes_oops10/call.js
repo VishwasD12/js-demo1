c367a0c9faa934cf8b9b30d268fc91d5b7599dd3
VishwasD12 <103741704+VishwasD12@users.noreply.github.com>
2025-03-10 15:02:49 +0000

    call

---
+++ classes_oops10/call.js
@@ -0,0 +1,15 @@
+function setUsername(username){
+    this.username=username
+    console.log("called");
+    
+}
+
+function createUser(username, email, password){
+    setUsername.call(this, username)
+
+    this.email=email
+    this.password=password
+}
+
+const user = new createUser("Vishwas", "vish@gmail.com", 123)
+console.log(user);
